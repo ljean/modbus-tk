5977a48ad8f807bdd9f195070016ed1c2d03463e
Bob Barcklay <bob@kisensum.com>
2016-04-30 14:49:34 -0700

    Remove the unnecessary time.sleep() in RtuMaster._send(). See Issue #57

--- modbus_tk/modbus_rtu.py
+++ modbus_tk/modbus_rtu.py
@@ -121,7 +121,6 @@ class RtuMaster(Master):
         self._serial.flushOutput()
 
         self._serial.write(request)
-        time.sleep(self.get_timeout())
 
     def _recv(self, expected_length=-1):
         """Receive the response from the slave"""
