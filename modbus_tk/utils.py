803bd9bad7dc4ca729c8703a5c2c8da1a4c30e0d
Bob Barcklay <bob@kisensum.com>
2016-09-19 20:38:31 -0700

    Allow caller to optionally override @threadsafe_function locking.

--- modbus_tk/utils.py
+++ modbus_tk/utils.py
@@ -25,14 +25,18 @@ def threadsafe_function(fcn):
     lock = threading.RLock()
 
     def new(*args, **kwargs):
-        """lock and call the decorated function"""
-        lock.acquire()
+        """Lock and call the decorated function
+
+           Unless kwargs['threadsafe'] == False
+        """
+        threadsafe = kwargs.pop('threadsafe', True)
+        if threadsafe: lock.acquire()
         try:
             ret = fcn(*args, **kwargs)
         except Exception as excpt:
             raise excpt
         finally:
-            lock.release()
+            if threadsafe: lock.release()
         return ret
     return new
 
